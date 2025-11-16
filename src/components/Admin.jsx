0b54f8a0fd1795c4f50dc5cbf7bdbef315397261
María Eugenia Barrena Reyes <mariaeugebrb@gmail.com>
2025-11-16 17:15:20 -0300

    Actualizo el componente Admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,26 +1,15 @@
-
 import { Link } from "react-router-dom";
-
 import GestionProducto from "./GestionProducto";
 
 const Admin = () => {
-
   return (
-    <div>
-       <div style={{ padding: "20px" }}></div>
-  
+    <div className="container mt-4">
       <h1>Gestión de Productos</h1>
       <p>Desde aquí podrías gestionar productos, revisar pedidos, o editar precios.</p>
-        <div>
-          <GestionProducto />
-        </div>
-
-        
-           
+      <GestionProducto />
     </div>
-
   );
 };
 
-
 export default Admin;
+
